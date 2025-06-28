dd390625166de084917086b4254b0833006027fd
Minsu Seo <lullulalal@gmail.com>
2025-06-28 15:47:34 +0200

    change the package name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:DokkaebieImage/main.dart';
+import 'package:dokkaebie_image/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

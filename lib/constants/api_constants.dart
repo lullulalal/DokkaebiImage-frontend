fd5857ee29bcd8c42231478a951aaba6a8e7443a
Minsu Seo <lullulalal@gmail.com>
2025-06-29 10:48:11 +0200

    change the baseUrl for deployment

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  //static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = 'https://138.199.215.9.nip.io';
 
   static const String colorTransfer = '$baseUrl/colorTransfer';
   static const String noiseRemover = '$baseUrl/noiseRemover';
